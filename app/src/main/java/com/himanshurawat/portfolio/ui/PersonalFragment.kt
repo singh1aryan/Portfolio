0fa7d854f8ddd676c7dd12b420c0b68421fdcf6a
Himanshu Rawat <rawath54@gmail.com>
2018-12-15 10:19:30 +0530

    Initial Commit

---
+++ app/src/main/java/com/himanshurawat/portfolio/ui/PersonalFragment.kt
@@ -0,0 +1,17 @@
+package com.himanshurawat.portfolio.ui
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.himanshurawat.portfolio.R
+
+class PersonalFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view =  inflater.inflate(R.layout.fragment_personal,container,false)
+
+        return view
+    }
+}
