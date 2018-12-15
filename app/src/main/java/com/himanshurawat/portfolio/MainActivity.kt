85361587003b66ae14e67029516f9387af59ae63
Himanshu Rawat <rawath54@gmail.com>
2018-12-15 10:50:49 +0530

    Migrated to Android X

--- app/src/main/java/com/himanshurawat/portfolio/MainActivity.kt
+++ app/src/main/java/com/himanshurawat/portfolio/MainActivity.kt
@@ -1,24 +1,40 @@
 package com.himanshurawat.portfolio
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.snackbar.Snackbar
+import androidx.fragment.app.FragmentManager
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.view.MotionEvent
+import android.view.View
+import android.widget.FrameLayout
+import com.himanshurawat.portfolio.ui.PersonalFragment
 
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnTouchListener {
+
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        if(v != null && v.id == activity_main_seekbar.id){
+            return true
+        }
+        return false
+    }
+
+    private lateinit var frameLayoutContainer: FrameLayout
+    private lateinit var fragmentManager: androidx.fragment.app.FragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        fragmentManager = supportFragmentManager
+        activity_main_seekbar.progress = 20
+        activity_main_seekbar.setOnTouchListener(this)
+        setup()
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -35,5 +51,11 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun setup(){
+        fragmentManager.beginTransaction().
+                replace(R.id.content_main_frame_layout_container,PersonalFragment(),"PersonalFragment").
+                commit()
     }
 }
--- app/src/main/java/com/himanshurawat/portfolio/ui/PersonalFragment.kt
+++ app/src/main/java/com/himanshurawat/portfolio/ui/PersonalFragment.kt
@@ -1,13 +1,13 @@
 package com.himanshurawat.portfolio.ui
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.himanshurawat.portfolio.R
 
-class PersonalFragment : Fragment() {
+class PersonalFragment : androidx.fragment.app.Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view =  inflater.inflate(R.layout.fragment_personal,container,false)
